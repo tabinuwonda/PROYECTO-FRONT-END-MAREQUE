a1ad2673b2b2ac6e44979224dcf6ee03349ece36
Tabatha Mareque <nuwonda.nuwonda@gmail.com>
2025-11-04 21:50:10 -0300

    añadi funciones al form

--- JS/scrip.js
+++ JS/scrip.js
@@ -7,3 +7,23 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('show'); // agrega o quita la clase 'show'
 });
+
+
+// --------------FORMULARIO---------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
+  const mensaje = document.getElementById("mensaje-exito");
+
+  form.addEventListener("submit", () => {
+    // Mostrar el mensaje de éxito cuando se envía
+    mensaje.style.display = "block";
+
+    // Limpiar los campos del formulario
+    form.reset();
+
+    
+  });
+});
+
+// -------------FIN FORMULARIO-----------------
